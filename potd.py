37272fb6eb9c45e5d2fd4f169b229ca8a7d08f59
alotofnekos <yukinefirablu@gmail.com>
2025-08-19 15:01:31 +0800

    cat cant count

--- potd.py
+++ potd.py
@@ -207,8 +207,8 @@ async def send_potd(ws, ROOM):
 async def build_daily_potd(ws, ROOM):
     """Send the POTD card every 2 hours to a room via ws."""
     while True:
-        await send_potd(ws, ROOM)
         await asyncio.sleep(2 * 60 * 60)  # wait 2 hours
+        await send_potd(ws, ROOM)
         
 
 
